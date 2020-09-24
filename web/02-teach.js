7f53c2717c20e4ae1dcb9cbb3ee1598f74429dac
Trevor <trevorjames02@gmail.com>
2020-09-23 23:26:49 -0600

    modified 02-teach files

--- web/02-teach.js
+++ web/02-teach.js
@@ -1,3 +1,8 @@
 function handleClick() {
     alert("Clicked!");
 }
+
+function changeColor() {
+    var color = document.getElementById("text1").innerHTML;
+    document.getElementById("div1").style.backgroundColor = color;
+}
